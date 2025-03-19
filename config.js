b61eb3b0605282a7181a3e8bd504b9d7737ef97c
redoan1 <64218246+redoan1@users.noreply.github.com>
2025-03-19 16:44:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://tanvir:tanvir@discordbot.7eiyk.mongodb.net/?retryWrites=true&w=majority",
+  mongodbUri : "mongodb+srv://razeen:<db_password>@cluster0.5iwt6.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
